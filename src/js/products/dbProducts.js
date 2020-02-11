e3a60f21ddea0886ac3bf8ca62008d0e9260a1c8
Róbert Fjölnir Birkisson <rfb2@hi.is>
2020-02-11 13:34:44 +0000

    Added dbproducts

---
+++ src/js/products/dbProducts.js
@@ -0,0 +1,51 @@
+const { query } = require('../db');
+
+
+export default async function selectProducts(params) {
+  const keys = Object.keys(params);
+
+  keys.map((key) => {
+    if (key === 'name') {
+      keys[key] = `%${keys[key].toLowerCase()}%`;
+    }
+    return null;
+  });
+
+  const paramValues = Object.values(params);
+
+  let searchParams = '';
+  keys.forEach((key, index) => {
+    let searchParam;
+    if (key === 'name') {
+      searchParam = `lower(title) LIKE $${index + 1}`;
+    } else if (key === 'code') {
+      searchParam = `lower(author) LIKE $${index + 1}`;
+    } else if (key === 'packaging') {
+      searchParam = `price >= $${index + 1}`;
+    } else if (key === 'origin') {
+      searchParam = `price <= $${index + 1}`;
+    } else if (key === 'grade') {
+      searchParam = `price = $${index + 1} `;
+    }
+
+    if (index === keys.length - 1) {
+      searchParams = searchParams.concat(searchParam);
+    } else {
+      searchParams = searchParams.concat(searchParam, ' AND ');
+    }
+  });
+
+  let q;
+  if (paramValues.length === 0) {
+    q = 'SELECT * FROM books';
+  } else {
+    q = `SELECT * FROM books WHERE 
+    ${searchParams}
+    `;
+  }
+
+  const result = await query(q, [
+    ...paramValues,
+  ]);
+  return result.rows;
+}
