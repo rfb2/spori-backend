6143e9720554af00a5aa3b9dad858a51c5b58a27
Róbert Fjölnir Birkisson <rfb2@hi.is>
2020-02-16 23:57:21 +0000

    Added another product

--- src/sql/products/insertProducts.sql
+++ src/sql/products/insertProducts.sql
@@ -1,1 +1,2 @@
 INSERT INTO products (name, code, packaging, origin, grade) VALUES ('Kókómjólk', '1234567890123', 'paper', 'Ísland', 5);
+INSERT INTO products (name, code, packaging, origin, grade) VALUES ('Slátur', '9876543219876', 'plastic', 'Ísland', 4);
