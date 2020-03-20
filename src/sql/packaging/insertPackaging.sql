1c7072fc4e92959b36627961b743887e1eac7af4
Róbert Fjölnir Birkisson <rfb2@hi.is>
2020-03-20 17:36:55 +0000

    Added coca cola

--- src/sql/packaging/insertPackaging.sql
+++ src/sql/packaging/insertPackaging.sql
@@ -1,2 +1,3 @@
 INSERT INTO packaging (name, footprint) VALUES ('plastic', 1.15);
 INSERT INTO packaging (name, footprint) VALUES ('paper', 12.025);
+INSERT INTO packaging (name, footprint) VALUES ('aluminium can', 11.09)
