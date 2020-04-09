2f2a000852f23e0e76b6ddebddb81348528e97fa
Róbert Fjölnir Birkisson <rfb2@hi.is>
2020-04-09 17:23:59 +0000

    Added a bunch of products

--- src/sql/products/insertProducts.sql
+++ src/sql/products/insertProducts.sql
@@ -5,4 +5,4 @@ INSERT INTO products (name, code, packaging, origin, score) VALUES ('Arna Léttm
 INSERT INTO products (name, code, packaging, origin, score) VALUES ('MS Léttmjólk 1l laktósafrí', '5690527115001', 'paper', 'Ísland', 4);
 INSERT INTO products (name, code, packaging, origin, score) VALUES ('MS G-mjólk 0.25l', '5690527200004', 'paper', 'Ísland', 4);
 INSERT INTO products (name, code, packaging, origin, score) VALUES ('Smjörvi', '5690516025007', 'paper', 'Ísland', 4);
-INSERT INTO products (name, code, packaging, origin, score) VALUES ('FK Brauð stórt', '5690568010396', 'paper', 'Ísland', 4);
+INSERT INTO products (name, code, packaging, origin, score) VALUES ('FK Brauð stórt', '5690568010396', 'plastic', 'Ísland', 4);
