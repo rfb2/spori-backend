3b7689b60ed25a4e2a9a0a2be8c5742496226d6a
Róbert Fjölnir Birkisson <rfb2@hi.is>
2020-04-09 17:23:31 +0000

    Added a bunch of products

--- src/sql/products/insertProducts.sql
+++ src/sql/products/insertProducts.sql
@@ -1,3 +1,8 @@
 INSERT INTO products (name, code, packaging, origin, score) VALUES ('Coca Cola 0.3l dós', '5449000000996', 'aluminium can', 'Ísland', 3);
-INSERT INTO products (name, code, packaging, origin, score) VALUES ('Léttmjólk 1l', '5690527190008', 'paper', 'Ísland', 4);
-INSERT INTO products (name, code, packaging, origin, score) VALUES ('Nýmjólk 1l', '5690527105002', 'paper', 'Ísland', 4);
+INSERT INTO products (name, code, packaging, origin, score) VALUES ('MS Léttmjólk 1l', '5690527190008', 'paper', 'Ísland', 4);
+INSERT INTO products (name, code, packaging, origin, score) VALUES ('MS Nýmjólk 1l', '5690527105002', 'paper', 'Ísland', 4);
+INSERT INTO products (name, code, packaging, origin, score) VALUES ('Arna Léttmjólk 1l', '5694230157396', 'paper', 'Ísland', 4);
+INSERT INTO products (name, code, packaging, origin, score) VALUES ('MS Léttmjólk 1l laktósafrí', '5690527115001', 'paper', 'Ísland', 4);
+INSERT INTO products (name, code, packaging, origin, score) VALUES ('MS G-mjólk 0.25l', '5690527200004', 'paper', 'Ísland', 4);
+INSERT INTO products (name, code, packaging, origin, score) VALUES ('Smjörvi', '5690516025007', 'paper', 'Ísland', 4);
+INSERT INTO products (name, code, packaging, origin, score) VALUES ('FK Brauð stórt', '5690568010396', 'paper', 'Ísland', 4);
